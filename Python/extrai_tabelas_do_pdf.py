b4ad154c97bece505f932c24409e7e24c3d97758
Bruno Fontes de Sousa <150109357+bruno-fsousa@users.noreply.github.com>
2024-05-28 08:34:50 -0300

    update script extrai_tabelas_do_pdf.py

--- Python/extrai_tabelas_do_pdf.py
+++ Python/extrai_tabelas_do_pdf.py
@@ -3,18 +3,23 @@
 # Instale as dependências
 # pip install tabula-py==2.9.3
 
+# Instale o java
+
 import os
 import tabula
+from pathlib import Path
 
 # Definir o caminho para o Java
 # IMPORTANTE: Adicionar a variável JAVA_HOME (com esse mesmo nome) e esse mesmo caminho nas variáveis de ambiente do Windows
-java_path = r'C:\Program Files (x86)\Common Files\Oracle\Java\java8path'
 
 # Definir a variável de ambiente JAVA_HOME
-os.environ["JAVA_HOME"] = java_path
+os.environ["JAVA_HOME"] = r'C:\Program Files\Common Files\Oracle\Java\javapath'
+
+# Pasta deste script
+dir_path = Path(__file__).parent
 
 # Caminho para o arquivo PDF
-pdf_path = "arquivo.pdf"
+pdf_path = dir_path.joinpath("2022_Soeharto_Csapo_Exploring_Indonesian_student_misconcepti.pdf")
 
 # Função para extrair tabelas do PDF
 def extract_tables(pdf_path):
@@ -33,7 +38,4 @@ for i, table in enumerate(tables):
     output_path = f'table_{i + 1}.csv'
     table.to_csv(output_path, index=False)  # Salva a tabela em um arquivo CSV sem índice
     print(f'Table {i + 1} saved to {output_path}')
-
-
-
 
